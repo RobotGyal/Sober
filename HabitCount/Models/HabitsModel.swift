d66fcef1607e76bb753df3c354e7662db26aad0e
mohammed <mdrame113@gmail.com>
2020-03-04 02:34:27 -0800

    Add new row in TableViw

---
+++ HabitCount/Models/HabitsModel.swift
@@ -0,0 +1,9 @@
+//
+//  HabitsModel.swift
+//  HabitCount
+//
+//  Created by Mohammed Drame on 3/4/20.
+//  Copyright © 2020 Mo Drame. All rights reserved.
+//
+
+import Foundation
---
+++ HabitCount/Views/TableViewCustomCell/CustomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomeTableViewCell.swift
+//  HabitCount
+//
+//  Created by Mohammed Drame on 3/4/20.
+//  Copyright © 2020 Mo Drame. All rights reserved.
+//
+
+import UIKit
+
+class CustomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
