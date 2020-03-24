eef8de4b6016b42caa8aa69b41369fe922b40bc3
mohammed <mdrame113@gmail.com>
2020-03-23 20:23:32 -0700

    welcome label and logoImage added

--- HabitCount/Controllers/LoginViewController.swift
+++ HabitCount/Controllers/LoginViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(welcomeBacklabel)
         welcomeBacklabelConstrain()
+        view.addSubview(logoImage)
+        logoImageConstraints()
     }
     
     // Todo ...
-    // 1. Create welcome back label
+    // 1. Create welcome back label ✅
     
-    // 2. Create bounce Title label
+    // 2. Create bounce Title label ✅
     
     // 3. Create logo imageView
     
@@ -43,19 +45,43 @@ class LoginViewController: UIViewController {
     
     lazy var welcomeBacklabel: UILabel = {
         let welcomeBacklabel = UILabel(frame: .zero)
-        welcomeBacklabel.text = "Welcome Back"
+        welcomeBacklabel.text = "Welcome Back ! ! !"
+        welcomeBacklabel.font = UIFont(name: "Arial", size: 25)
+        welcomeBacklabel.translatesAutoresizingMaskIntoConstraints = false
         return welcomeBacklabel
     }()
     
     func welcomeBacklabelConstrain() {
         NSLayoutConstraint.activate([
-            welcomeBacklabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
-            welcomeBacklabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            welcomeBacklabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -self.view.frame.size.height / 5),
-            welcomeBacklabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+            welcomeBacklabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 80),
+            welcomeBacklabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
         ])
     }
     
+    lazy var logoImage: UIImageView = {
+        let logoImage = UIImageView(frame: .zero)
+        logoImage.contentMode = .scaleAspectFit
+        logoImage.translatesAutoresizingMaskIntoConstraints = false
+        logoImage.image = #imageLiteral(resourceName: "lotus")
+        return logoImage
+    }()
     
-
+    func logoImageConstraints() {
+            NSLayoutConstraint.activate([
+                logoImage.topAnchor.constraint(equalTo: welcomeBacklabel.topAnchor, constant: 60),
+                logoImage.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
+                logoImage.widthAnchor.constraint(equalTo: view.widthAnchor, constant: 4),
+                logoImage.heightAnchor.constraint(equalToConstant: 200)
+    //            pickedImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 70),
+    //            pickedImage.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
+    //            pickedImage.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -70)
+            ])
+        }
+    
+    
+    
+    
+    
+    
+    
 }
